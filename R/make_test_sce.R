ce77be02124602767dec7c9b7e4b2ffbd22e8b7e
Louise Huuki <lahuuki@gmail.com>
2021-01-27 12:37:55 -0500

    Add make_test_sce & update documentation

---
+++ R/make_test_sce.R
@@ -0,0 +1,34 @@
+
+#' Title
+#'
+#' @param n_cell Number of cells
+#' @param n_gene Number of genes
+#' @param n_cellType Number of cell types
+#' @param n_donor Number of donors
+#'
+#' @return SingelCellExperiment object with randomly generated counts and colData
+#' @export
+#'
+#' @examples
+#' test <- make_test_sce()
+#' table(test$cellType, test$donor)
+#'
+make_test_sce <- function(n_cell = 100, n_gene = 100, n_cellType = 4, n_donor = 2){
+
+  counts <- matrix(rpois(n_cell*n_gene, lambda = 10), ncol= n_cell, nrow = n_gene)
+  sce <- SingleCellExperiment(list(counts=counts))
+
+  donors <- paste0("D",1:n_donor)
+  cells <- paste0("C",1:n_cellType)
+
+  pd <- DataFrame(donor = sample(donors, n_cell, replace = TRUE),
+                   cellType = sample(cells, n_cell, replace = TRUE))
+
+  width <- nchar(as.character(n_cell))
+  rownames(pd) <- paste0("S",str_pad(1:n_cell, width = width, pad = "0"))
+  colData(sce) <- pd
+
+  rownames(sce) <- paste0("G",1:n_gene)
+
+  return(sce)
+}
