3075f74738a361727211fd78c7a0c5f94e02dbdc
Louise Huuki <lahuuki@gmail.com>
2021-01-28 15:38:21 -0500

    Fix cellType and gene col issues, update colnames

--- R/get_mean_ratio2.R
+++ R/get_mean_ratio2.R
@@ -15,47 +15,50 @@
 #'
 #'@importFrom dplyr mutate
 #'@importFrom purrr map
+#'@importFrom purrr map2
 #'@importFrom jaffelab ss
 get_mean_ratio2 <- function(sce, cellType_col =  "cellType", assay_name = "counts", add_symbol = FALSE){
 
-   celltypes <- unique(sce[[cellType_col]])
-   names(celltypes) <- celltypes
+   cell_types <- unique(sce[[cellType_col]])
+   names(cell_types) <- cell_types
 
    sce_assay <- as.matrix(assays(sce)[[assay_name]])
 
-   cell_means <- map(celltypes, ~as.data.frame(rowMeans(sce_assay[,sce[[cellType_col]] == .x])))
-   # edit cell means table
+   ## Get mean expression for each gene for each cellType
+   cell_means <- map(cell_types, ~as.data.frame(base::rowMeans(sce_assay[,sce[[cellType_col]] == .x])))
+
    cell_means <- do.call("rbind", cell_means)
    colnames(cell_means) <- "mean"
-   cell_means$cellType = ss(rownames(cell_means),"\\.")
-   cell_means$gene = ss(rownames(cell_means),"\\.",2)
+   ## Define columns
+   cell_means$cellType = rep(cell_types, each = nrow(sce))
+   cell_means$gene = rep(rownames(sce),length(cell_types))
+   # print(head(cell_means))
 
-
-   ratio_tables <- map(celltypes, function(x){
+   ## Filter and calculate ratio for each celltype
+   ratio_tables <- map(cell_types, function(x){
       #filter target median != 0
       median_index <- rowMedians(sce_assay[,sce[[cellType_col]] == x]) != 0
       # message("Median == 0: ", sum(!median_index))
       #filter for target means
       target_mean <- cell_means[cell_means$cellType == x,]
       target_mean <- target_mean[median_index,]
-      colnames(target_mean) <- c("target_mean","target_cellType","gene")
+      colnames(target_mean) <- c("mean.target","cellType.target","gene")
 
       nontarget_mean <- cell_means[cell_means$cellType != x,]
 
 
       dplyr::left_join(target_mean, nontarget_mean, by = "gene") %>%
-         mutate(ratio = target_mean/mean) %>%
+         mutate(ratio = mean.target/mean) %>%
          dplyr::group_by(gene) %>%
          arrange(ratio) %>%
          slice(1) %>%
-         select(gene, target_cellType, target_mean, cellType, mean, ratio) %>%
+         select(gene, cellType.target, mean.target, cellType, mean, ratio) %>%
          arrange(-ratio) %>%
          dplyr::ungroup() %>%
-         mutate(ratio_rank = dplyr::row_number())
+         mutate(rank_ratio = dplyr::row_number())
 
    })
 
    ratio_tables <- do.call("rbind", ratio_tables)
-
    return(ratio_tables)
 }
