ff1b13e590d8a7fbb041b64cac43ae8d1ca7f5be
Louise Huuki <lahuuki@gmail.com>
2021-02-08 16:04:29 -0500

    Add sce_ab data for testing pseudobulk code

---
+++ data-raw/sce_ab.R
@@ -0,0 +1,15 @@
+## code to prepare `sce_ab` dataset goes here
+
+pd <- S4Vectors::DataFrame(donor = rep(c("D1","D2"), each = 50),
+                           cellType = rep(rep(c("A","B"), each = 25), 2))
+rownames(pd) <- paste0("S",stringr::str_pad(1:nrow(pd), width = nchar(nrow(pd)), pad = "0"))
+
+combo <- paste0(pd$donor,"_",pd$cellType)
+tc <- table(combo)
+
+counts <- do.call(rbind, map(names(tc), ~as.integer(.x == combo)))
+rownames(counts) <- paste0("G-", names(tc))
+sce_ab <- SingleCellExperiment::SingleCellExperiment(list(counts=counts))
+colData(sce_ab) <- pd
+
+usethis::use_data(sce_ab, overwrite = TRUE)
