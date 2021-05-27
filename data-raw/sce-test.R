7dffccbd987b75c7b302dac073e212ebea2b641a
Louise Huuki <lahuuki@gmail.com>
2021-05-27 11:57:24 -0400

    Get sce.test < 5Mb

--- data-raw/sce-test.R
+++ data-raw/sce-test.R
@@ -1,3 +1,18 @@
 ## code to prepare `sce.test` dataset goes here
 load(here::here("data", "sce.test.Rdata"), verbose = TRUE)
+print(object.size(sce.test), units = "auto")
+dim(sce.test)
+table(sce.test$cellType)
+## Drop Excit 3 & 4
+sce.test <- sce.test[,!sce.test$cellType %in% c("Excit.3","Excit.4")]
+sce.test$cellType <- droplevels(sce.test$cellType)
+
+## Drop Oligo down to ~ 500 cells
+oligo_filter <- !sce.test$cellType == "Oligo"
+oligo_filter[!oligo_filter] <- sample(c(TRUE, rep(FALSE,5)), sum(!oligo_filter),replace = TRUE)
+
+sce.test <- sce.test[,oligo_filter]
+## select 500 cells
+sce.test <- sce.test[,sample(colnames(sce.test), 500)]
+print(object.size(sce.test), units = "auto")
 usethis::use_data(sce.test, overwrite = TRUE)
