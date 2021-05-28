4335e01efddb23b0f67cedfda5a688d7922ac437
Louise Huuki <lahuuki@gmail.com>
2021-05-28 13:02:04 -0400

    Document Data

---
+++ R/data.R
@@ -0,0 +1,58 @@
+
+#' List of ages defining life stages
+#'
+#' A named list of the minimum and maximum ages for 6 life stages (Prenatal, 
+#' Infant, Child, Teen, Adult, and 50+). Used in `add_age_group()`.
+#'
+#' @format Named list.
+#' @source <https://github.com/LieberInstitute/DeconvoBuddies/blob/main/data-raw/age_categories.R>
+"age_categories" 
+
+#' Test Estimated Cell Type Proportions
+#'
+#' A test dataset of estiamted protions for 5 cell types over 100 samples. 
+#'
+#' @format A data.frame object.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/est_prop.R>
+"est_prop" 
+
+#' Markers stats from sce.test
+#'
+#' A tibble containing the marker stats from `get_mean_ratio2` for `sce.test`. 
+#'
+#' @format tibble.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/marker_test.R>
+"marker_test" 
+
+#' Test bulk rse dataset 
+#'
+#' TODO Description.
+#'
+#' @format A SummarizedExperiment object.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/genotyped.R>
+"rse_bulk_test" 
+
+#' TODO genotyped
+#'
+#' TODO Description.
+#'
+#' @format A SingleCellExperiment object.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/sce.test.R>
+"sce.test" 
+
+#' Toy SCE object for testing
+#'
+#' TODO Description.
+#'
+#' @format A SingleCellExperiment object.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/sce_ab.R>
+"sce_ab" 
+
+#' A 20 color pallet
+#'
+#' Adapted from `scater`
+#'
+#' @format list of 20 hex colors
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/tableau20.R>
+"tableau20" 
+
