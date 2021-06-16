3fdfd65cc19d64f37cc3d2ad47f02c37ef5278cd
Louise Huuki <lahuuki@gmail.com>
2021-06-16 09:31:12 -0400

    Prep all sysdata in one script, sava as R/sysdata.rda

--- data-raw/tableau20.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `tableau20` dataset goes here
-
-### Palette taken from `scater`
-tableau20 = c("#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C",
-              "#98DF8A", "#D62728", "#FF9896", "#9467BD", "#C5B0D5",
-              "#8C564B", "#C49C94", "#E377C2", "#F7B6D2", "#7F7F7F",
-              "#C7C7C7", "#BCBD22", "#DBDB8D", "#17BECF", "#9EDAE5")
-
-usethis::use_data(tableau20, overwrite = TRUE, internal = TRUE)
