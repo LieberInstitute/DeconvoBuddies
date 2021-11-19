874409f9ab401c22dd7f27c1c514c1184f6a0a32
Louise Huuki <lahuuki@gmail.com>
2021-11-19 09:36:45 -0500

    Fix pseudobulk test

--- tests/testthat/test-pseudobulk.R
+++ tests/testthat/test-pseudobulk.R
@@ -1,4 +1,4 @@
-pb_ab <- pseudobulk(sce_ab, cell_group_cols = c("donor", "cellType"))
+pb_ab <- pseudobulk(sce_ab, cell_group_cols = c("donor", "cellType"), just_counts = TRUE)
 
 test_that("Correct Dim", {
     expect_equal(nrow(pb_ab), nrow(sce_ab))
