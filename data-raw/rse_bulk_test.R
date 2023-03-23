c8bdb98ed30fe8e75da44d0e6f42b0c615ef8d7f
Louise Huuki <lahuuki@gmail.com>
2023-03-23 15:42:14 -0400

    Make rse_bulk_test smaller to reduce tarball size

--- data-raw/rse_bulk_test.R
+++ data-raw/rse_bulk_test.R
@@ -1,24 +1,24 @@
 ## code to prepare `rse_bulk_test` dataset goes here
 set.seed(1234)
 
-nrows <- 2000
+nrows <- 1000
 ncols <- 100
 counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
 
 gene_sym <- paste0("G", stringr::str_pad(seq(nrows), nchar(nrows), side = "left", pad = 0))
 
-rowRanges <- GRanges(rep(c("chr1", "chr2"), c(500, nrows - 500)),
-    IRanges(floor(runif(nrows, 1e5, 1e6)), width = 100),
-    strand = sample(c("+", "-"), nrows, TRUE),
-    feature_id = sprintf("ID%03d", seq(nrows)),
-    Symbol = gene_sym
+rowRanges <- GenomicRanges::GRanges(rep(c("chr1", "chr2"), c(500, nrows - 500)),
+                                    IRanges::IRanges(floor(runif(nrows, 1e5, 1e6)), width = 100),
+                                    strand = sample(c("+", "-"), nrows, TRUE),
+                                    feature_id = sprintf("ID%03d", seq(nrows)),
+                                    Symbol = gene_sym
 )
 
 BrNums <- paste0("Br", stringr::str_pad(1:ncols, nchar(ncols), side = "left", pad = 0))
 RNums_raw <- sample(seq((ncols * 10)), ncols, replace = FALSE)
 RNums <- paste0("R", stringr::str_pad(RNums_raw, nchar(max(RNums_raw)), side = "left", pad = 0))
 
-colData <- DataFrame(
+colData <- S4Vectors::DataFrame(
     RNum = RNums,
     BrNum = BrNums,
     Sex = sample(c("M", "F"), ncols, replace = TRUE),
@@ -26,7 +26,7 @@ colData <- DataFrame(
     Age = runif(ncols, min = -1, max = 100)
 )
 
-rse_bulk_test <- SummarizedExperiment(
+rse_bulk_test <- SummarizedExperiment::SummarizedExperiment(
     assays = list(counts = counts),
     rowRanges = rowRanges, colData = colData
 )
