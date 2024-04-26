09163d7b345a2ed4eb94bc72dd5041c93048688d
Louise Huuki <lahuuki@gmail.com>
2024-04-26 14:36:18 -0400

    Update mean_ratio tests

--- tests/testthat/test-get_mean_ratio2.R
+++ tests/testthat/test-get_mean_ratio2.R
@@ -1,14 +1,14 @@
-mrt <- suppressMessages(get_mean_ratio(sce.test, assay_name = "logcounts", cellType_col = "cellType", add_symbol = TRUE))
+mrt <- suppressMessages(get_mean_ratio(sce.test, assay_name = "logcounts", cellType_col = "cellType", gene_name = "Symbol"))
 mrt2 <- get_mean_ratio2(sce.test, assay_name = "logcounts", cellType_col = "cellType", add_symbol = TRUE)
 
 test_that("Correct Dims", {
     expect_equal(nrow(mrt2), nrow(mrt))
-    expect_equal(ncol(mrt2) + 2, ncol(mrt))
+    expect_equal(ncol(mrt2), ncol(mrt))
 })
 
 r1 <- mrt %>% dplyr::filter(gene == "ENSG00000187147", cellType.target == "Oligo")
-r2 <- mrt %>% dplyr::filter(gene == "ENSG00000187147", cellType.target == "Oligo")
+r2 <- mrt2 %>% dplyr::filter(gene == "ENSG00000187147", cellType.target == "Oligo")
 
 test_that("One Ratio Matches", {
-    expect_equal(r2$ratio, r1$ratio)
+    expect_equal(r2$ratio, r1$MeanRatio)
 })
