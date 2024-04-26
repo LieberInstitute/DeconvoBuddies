5f15bb5ef681b1bd0094d82848c99862baf537a5
Louise Huuki <lahuuki@gmail.com>
2024-04-26 14:30:56 -0400

    Update test with new params

--- tests/testthat/test-get_mean_ratio.R
+++ tests/testthat/test-get_mean_ratio.R
@@ -1,5 +1,5 @@
-mrt_ab <- suppressMessages(get_mean_ratio(sce_ab))
+mrt_ab <- suppressMessages(get_mean_ratio(sce_ab, cellType_col = "cellType"))
 
 test_that("Means all zero", {
-    expect_equal(sum(mrt_ab$mean), 0)
+    expect_equal(sum(mrt_ab$mean.2nd), 0)
 })
