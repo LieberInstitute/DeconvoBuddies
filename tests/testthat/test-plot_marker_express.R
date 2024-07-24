407bcfdcdb67468f7167c461223c5af34f72a3bc
Louise Huuki <lahuuki@gmail.com>
2024-07-24 07:50:17 -0400

    Update test data to sce_DLPF_example

--- tests/testthat/test-plot_marker_express.R
+++ tests/testthat/test-plot_marker_express.R
@@ -1,4 +1,6 @@
+if (!exists("sce_DLPFC_example")) sce_DLPFC_example <- fetch_deconvo_data("sce_DLPFC_example")
+
 test_that("Data checks", {
-    expect_error(plot_marker_express(sce = sce.test, cellType_col = "not_there", stat = marker_test, cell_type = "Astro"))
-    expect_error(plot_marker_express(sce = sce.test, stat = marker_test, cell_type = "not_there"))
+    expect_error(plot_marker_express(sce = sce_DLPFC_example, cellType_col = "not_there", stat = marker_test, cell_type = "Astro"))
+    expect_error(plot_marker_express(sce = sce_DLPFC_example, stat = marker_test, cell_type = "not_there"))
 })
