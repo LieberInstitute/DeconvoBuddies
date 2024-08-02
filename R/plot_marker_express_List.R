2d2da2fa90b531da5048562c5615730e69c763fd
Louise Huuki <lahuuki@gmail.com>
2024-08-02 15:14:34 -0400

    Add pdf_fn=NULL option for plot_marker_express ALL and List

--- R/plot_marker_express_List.R
+++ R/plot_marker_express_List.R
@@ -6,12 +6,12 @@
 #'
 #' @param sce [SummarizedExperiment-class][SummarizedExperiment::SummarizedExperiment-class] object
 #' @param gene_list A named list containing the names of genes to plot
-#' @param pdf_fn A `character()` of the pdf file name to plot to
 #' @param cellType_col The `character()` name of `colData` column containing cell type for `sce` data
 #' @param gene_name_col The `character()` name of `rowData` matching the gene name from `gene_list`
 #' @param color_pal  A named `character(1)` vector that contains a color pallet matching the values in `cellType_col`.
 #' @param plot_points A logical indicating whether to plot points over the violin,
 #' defaults to `FALSE` as these often become over plotted and quite large (especially when saved as PDF)
+#' #' @inheritParams pdf_fn
 #'
 #' @return A pdf with violin plots for the expression of top marker genes for all cell types
 #'
@@ -23,7 +23,15 @@
 #'
 #' ## Create list-of-lists of genes to plot, names of sub-list become title of page
 #' my_gene_list <- list(Inhib = c("GAD2", "SAMD5"), Astro = c("RGS20", "PRDM16"))
-#'
+#' 
+#' # Return a list of plots 
+#' plots <- plot_marker_express_List(
+#'     sce_DLPFC_example,
+#'     gene_list = my_gene_list,
+#'     cellType_col = "cellType_broad_hc")
+#'     
+#' print(plots[[1]])
+#'     
 #' # Plot marker gene expression to PDF, one page per cell type in stats
 #' pdf_file <- tempfile("test_marker_expression_List", fileext = ".pdf")
 #'
@@ -35,13 +43,14 @@
 #' )
 #'
 #' if (interactive()) browseURL(pdf_file)
+#' 
 #' @family expression plotting functions
 #' @importFrom ggplot2 ggplot geom_violin geom_text facet_wrap stat_summary
 #' @importFrom SummarizedExperiment colData
 plot_marker_express_List <- function(
         sce,
         gene_list,
-        pdf_fn = "marker_expression.pdf",
+        pdf_fn = NULL,
         cellType_col = "cellType",
         gene_name_col = "gene_name",
         color_pal = NULL,
@@ -53,22 +62,24 @@ plot_marker_express_List <- function(
         rownames(sce) <- SummarizedExperiment::rowData(sce)[[gene_name_col]]
     }
 
-    grDevices::pdf(pdf_fn)
-    message("Plotting Genes for...")
-
-    for (i in seq(length(gene_list))) {
-        n <- names(gene_list)[[i]]
-        message("\t", n)
-        print(plot_gene_express(
-            sce = sce,
-            genes = gene_list[[i]],
-            cat = cellType_col,
-            color_pal = color_pal,
-            plot_points = plot_points,
-            title = n
-        ))
+    marker_plots <- purrr::map2(
+      gene_list, names(gene_list),
+      ~ plot_gene_express(
+        sce = sce,
+        genes = .x,
+        cat = cellType_col,
+        color_pal = color_pal,
+        plot_points = plot_points,
+        title = .y
+      )
+    )
+    
+    if(is.null(pdf_fn)){
+      return(marker_plots)
+    } else {
+      grDevices::pdf(pdf_fn)
+      print(marker_plots)
+      grDevices::dev.off()
     }
-
-    message("Done!")
-    grDevices::dev.off()
+    
 }
