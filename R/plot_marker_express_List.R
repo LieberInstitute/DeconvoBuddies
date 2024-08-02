efd959f7b09b536c3b061e6f970463a62cccbc91
Louise Huuki <lahuuki@gmail.com>
2024-08-02 15:32:47 -0400

    Fix inheritParams

--- R/plot_marker_express_List.R
+++ R/plot_marker_express_List.R
@@ -11,7 +11,7 @@
 #' @param color_pal  A named `character(1)` vector that contains a color pallet matching the values in `cellType_col`.
 #' @param plot_points A logical indicating whether to plot points over the violin,
 #' defaults to `FALSE` as these often become over plotted and quite large (especially when saved as PDF)
-#' #' @inheritParams pdf_fn
+#' #' @inheritParams plot_marker_express_ALL
 #'
 #' @return A pdf with violin plots for the expression of top marker genes for all cell types
 #'
