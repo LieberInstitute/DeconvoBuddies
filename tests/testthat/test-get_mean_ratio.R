31e5999e90d6993a4e6342fa681c473fcaa8296e
Louise Huuki <lahuuki@gmail.com>
2024-08-05 15:03:20 -0400

    Add test for  low cell warming

--- tests/testthat/test-get_mean_ratio.R
+++ tests/testthat/test-get_mean_ratio.R
@@ -3,3 +3,9 @@ mrt_ab <- suppressMessages(get_mean_ratio(sce_ab, cellType_col = "cellType"))
 test_that("Means all zero", {
     expect_equal(sum(mrt_ab$mean.2nd), 0)
 })
+
+if (!exists("sce_DLPFC_example")) sce_DLPFC_example <- fetch_deconvo_data("sce_DLPFC_example")
+
+test_that("Warn for <10 cells",{
+  expect_warning(get_mean_ratio(sce_DLPFC_example, cellType_col = "cellType_hc"))
+})
