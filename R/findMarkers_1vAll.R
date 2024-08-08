0803a031f05fec72cf823ea26e5ebb226c27afa8
Louise Huuki <lahuuki@gmail.com>
2024-08-08 13:56:05 -0400

    Update docs and colnames for findMarkers_1vAll

--- R/findMarkers_1vAll.R
+++ R/findMarkers_1vAll.R
@@ -5,21 +5,55 @@
 #' @param assay_name Name of the assay to use for calculation
 #' @param cellType_col Column name on colData of the sce that denotes the celltype
 #' @param add_symbol Add the gene symbol column to the marker stats table
-#' @param mod String specifying the model used as design in findMarkers. Can be `NULL` if there are no blocking terms with uninteresting factors as documented at [pairwiseTTests][scran::pairwiseTTests].
+#' @param mod String specifying the model used as design in findMarkers. 
+#' Can be `NULL` (default) if there are no blocking terms with uninteresting 
+#' factors as documented at [pairwiseTTests][scran::pairwiseTTests].
 #' @param verbose Boolean choosing to print progress messages or not
 #'
-#' @return Table of 1 vs. ALL std log fold change + p-values for each gene x cell type
+#' @return Tibble of 1 vs. ALL std log fold change + p-values for each gene x cell type
+#' -   `gene` is the name of the gene (from rownames(`sce`)).
+#' -   `logFC` the log fold change from the DE test
+#' -   `log.p.value` the log of the p-value of the DE test
+#' -   `log.FDR` the log of the False Discovery Rate adjusted p.value
+#' -   `std.logFC` the standard logFC
+#' -   `cellType.target` the cell type we're finding marker genes for
+#' -   `std.logFC.rank` the rank of `std.logFC` for each cell type
+#' -   `std.logFC.anno` is an annotation of the `std.logFC` value
+#'    helpful for plotting.
 #' @export
 #'
 #' @examples
-#' markers_1vAll <- findMarkers_1vAll(sce_ab)
-#' head(markers_1vAll)
+#' ## load example SingleCellExperiment
+#' if (!exists("sce_DLPFC_example")) sce_DLPFC_example <- fetch_deconvo_data("sce_DLPFC_example")
+#' ## Explore properties of the sce object
+#' sce_DLPFC_example
+#'
+#' ## this data contains logcounts of gene expression
+#' SummarizedExperiment::assays(sce_DLPFC_example)$logcounts[1:5, 1:5]
+#'
+#' ## nuclei are classified in to cell types
+#' table(sce_DLPFC_example$cellType_broad_hc)
+#'
+#' ## Get the 1vALL stats for each gene for each cell type defined in `cellType_broad_hc`
+#' marker_stats_1vAll <- findMarkers_1vAll(
+#'  sce = sce_DLPFC_example,
+#'  assay_name = "logcounts",
+#'  cellType_col = "cellType_broad_hc",
+#'  mod = "~BrNum"
+#' )
+#' 
+#' head(marker_stats_1vAll)
 #'
 #' @importFrom purrr map
 #' @importFrom dplyr mutate
 #' @importFrom scran findMarkers
 #' @importFrom tibble rownames_to_column as_tibble add_column
-findMarkers_1vAll <- function(sce, assay_name = "counts", cellType_col = "cellType", add_symbol = FALSE, mod = "~donor", verbose = TRUE) {
+findMarkers_1vAll <- function(sce, 
+                              assay_name = "counts", 
+                              cellType_col = "cellType", 
+                              add_symbol = FALSE,
+                              mod = NULL, 
+                              verbose = TRUE) {
     # RCMD Fix
     gene <- rank_marker <- cellType.target <- std.logFC <- rowData <- Symbol <- NULL
 
@@ -36,7 +70,7 @@ findMarkers_1vAll <- function(sce, assay_name = "counts", cellType_col = "cellTy
     }
 
     markers.t.1vAll <- map(cell_types, function(x) {
-        if (verbose) message(x, " - '", Sys.time())
+        if (verbose) message(Sys.time(), " - Find markers for: ", x)
 
         sce$contrast <- ifelse(sce[[cellType_col]] == x, 1, 0)
 
@@ -68,8 +102,8 @@ findMarkers_1vAll <- function(sce, assay_name = "counts", cellType_col = "cellTy
         tibble::add_column(cellType.target = rep(cell_types, each = nrow(sce))) |>
         dplyr::group_by(cellType.target) |>
         mutate(
-            rank_marker = dplyr::row_number(),
-            anno_logFC = paste0(" std logFC = ", round(std.logFC, 3))
+            std.logFC.rank = dplyr::row_number(),
+            std.logFC.anno = paste0(" std logFC = ", round(std.logFC, 3))
         )
 
     if (add_symbol) {
