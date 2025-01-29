0d76b63c8301af539fd17d22822939eee4c43689
Louise Huuki <lahuuki@gmail.com>
2025-01-29 15:21:17 -0500

    Use broad/fine cell types for split, return nested list of broad & fine color pallets. Flip plot, only plot fine ct when split. Update docs.

--- R/create_cell_colors.R
+++ R/create_cell_colors.R
@@ -17,7 +17,8 @@
 #' provided, overrides pallet selection with pallet_name.
 #' @param split delineating `character(1)` after which suffixes will be ignored.
 #' This is useful for cases when say `A.1` and `A.2` are both to be considered
-#' subtypes of cell type `A` (here `split = "\\."`).
+#' fine subtypes of broad cell type `A` (here `split = "\\."`). When used the 
+#' function returns a nested list of borad and fine cell types. 
 #' @param preview A `logical(1)` indicating whether to make a plot to preview
 #' the colors.
 #'
@@ -43,15 +44,16 @@
 #'                    previe = TRUE)
 #' 
 #' ## Options for subtype handling
-#' ## Provide unique colors for cell subtypes (DEFAULT)
+#' ## Provide unique colors for cell subtypes (DEFAULT) - returns one level list
 #' create_cell_colors(
 #'     cell_types = c("A.1", "A.2", "B.1", "C", "D"),
 #'     pallet_name = "classic",
-#'     preview = TRUE
+#'     preview = FALSE
 #' )
 #'
 #' ## Provide gradient colors for A.1 and A.2 by using the "split" argument
-#' ## returns a base cell type color & subtype colors
+#' ## returns a nested list with broad & fine cell type colors, fine cell types
+#' ## are gradient with the top level matching the broad cell type
 #' create_cell_colors(
 #'     cell_types = c("A.1", "A.2", "B.1", "C", "D"),
 #'     split = "\\.",
@@ -93,9 +95,8 @@ create_cell_colors <- function(
   stopifnot(length(cell_types) > 0)
   stopifnot(is.character(cell_types))
   
-  base_cell_types <- unique(ss(cell_types, pattern = split))
-  nct <- length(base_cell_types)
-  # if (nct < 3) stop("Need 3 or more base cell types")
+  broad_cell_types <- unique(ss(cell_types, pattern = split))
+  nct <- length(broad_cell_types)
   cell_colors <- list()
   
   ## check pallet selection
@@ -105,11 +106,11 @@ create_cell_colors <- function(
   } else if(!is.null(pallet)){ ## use custom pallet
     stopifnot(is.character(pallet))
     cell_colors = pallet
-    message(sprintf("Creating custom pallet for %d cell types", nct))
+    message(sprintf("Creating custom pallet for broad %d cell types", nct))
     
   } else { ## use user provided pallet
     pallet_name <- match.arg(pallet_name)
-    message(sprintf("Creating %s pallet for %d cell types", pallet_name, nct))
+    message(sprintf("Creating %s pallet for broad %d cell types", pallet_name, nct))
     
     if (pallet_name == "gg") {
       cell_colors <- gg_color_hue(nct)
@@ -136,36 +137,42 @@ create_cell_colors <- function(
     cell_colors <- cell_colors[seq(nct)]
   }
   ## assign cell types to colors
-  names(cell_colors) <- base_cell_types
+  names(cell_colors) <- broad_cell_types
   
   ## handle cell subtype gradient
-  if (!identical(base_cell_types, cell_types)) {
-    split_cell_types <- cell_types[!cell_types %in% base_cell_types]
-    base_split <- rafalib::splitit(ss(split_cell_types, split))
+  if (!identical(broad_cell_types, cell_types)) {
+    split_cell_types <- cell_types[!cell_types %in% broad_cell_types]
+    broad_split <- rafalib::splitit(ss(split_cell_types, split))
     
     split_scale_colors <- purrr::map2(
-      names(base_split), base_split,
+      names(broad_split), broad_split,
       ~ .scale_cell_colors(
         cell_colors[[.x]],
         split_cell_types[.y]
       )
     )
-    message(sprintf("Creating subtype gradients for %d base cell types", length(split_scale_colors)))
+    message(sprintf("Creating fine cell type gradients for %d cell types", length(split_scale_colors)))
     split_scale_colors <- unlist(split_scale_colors)
     cell_colors <- c(cell_colors, split_scale_colors)
+    cell_colors <- list(broad = cell_colors[broad_cell_types],
+                        fine = cell_colors[cell_types])
+    plot_cell_colors <- cell_colors$fine
+  } else {
+    plot_cell_colors <- cell_colors
   }
   
   ## plot preview
   if (preview) {
+    plot_cell_colors <- rev(plot_cell_colors) # flip order
     par(las = 2) # make label text perpendicular to axis
     par(mar = c(5, 8, 4, 2)) # increase y-axis margin.
-    bp <- barplot(rep(1, length(cell_colors)),
-            col = cell_colors,
+    bp <- barplot(rep(1, length(plot_cell_colors)),
+            col = plot_cell_colors,
             horiz = TRUE,
             axes = FALSE,
-            names.arg = names(cell_colors)
+            names.arg = names(plot_cell_colors)
     )
-    text(y = bp, x = rep(.5, length(cell_colors)), cell_colors)
+    text(y = bp, x = rep(.5, length(cell_colors)), plot_cell_colors)
   }
   
   return(cell_colors)
