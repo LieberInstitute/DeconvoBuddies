42b6a8b182ef7fe4325e5401933524a5fcb69125
Louise Huuki <lahuuki@gmail.com>
2025-02-17 11:05:05 +0000

    Add test for 1vALL

---
+++ tests/testthat/test-findMarkers_1vAll.R
@@ -0,0 +1,13 @@
+## example data
+if (!exists("sce_DLPFC_example")) sce_DLPFC_example <- fetch_deconvo_data("sce_DLPFC_example")
+marker_stats_1vAll <- findMarkers_1vAll(
+  sce = sce_DLPFC_example,
+  assay_name = "logcounts",
+  cellType_col = "cellType_broad_hc",
+  mod = "~BrNum"
+)
+
+test_that("1vALL stats returned for each gene", {
+  gene_count <- marker_stats_1vAll |> dplyr::count(cellType.target)
+  testthat::expect_true(all(gene_count$n == nrow(sce_DLPFC_example)))
+})
