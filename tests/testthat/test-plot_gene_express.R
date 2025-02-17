1c375e29bb60d716fd0da139925052a0388a9c56
Louise Huuki <lahuuki@gmail.com>
2025-02-17 11:41:28 +0000

    Test for expected labels in plot_gene functions

---
+++ tests/testthat/test-plot_gene_express.R
@@ -0,0 +1,11 @@
+data(sce_ab)
+test_plot <- plot_gene_express(sce = sce_ab, genes = c("G-D1_A"))
+
+test_that("Error with Missing Gene", {
+  expect_error(plot_gene_express(sce = sce_ab, genes = c("MISSING_GENE")))
+})
+
+
+test_that("Expected y-axis label",{
+  expect_identical(test_plot$labels$y, "Expression (logcounts)")
+})
--- tests/testthat/test-plot_marker_express.R
+++ tests/testthat/test-plot_marker_express.R
@@ -1,6 +1,22 @@
 if (!exists("sce_DLPFC_example")) sce_DLPFC_example <- fetch_deconvo_data("sce_DLPFC_example")
 
-test_that("Data checks", {
+data("marker_test")
+
+## Example plot
+test_plot <- plot_marker_express(
+  sce = sce_DLPFC_example,
+  stat = marker_test,
+  cellType_col = "cellType_broad_hc",
+  cell_type = "Astro",
+  gene_col = "gene"
+)
+
+test_that("Expected labels",{
+  expect_identical(test_plot$labels$title, "Astro Top 4 Markers")
+  expect_identical(test_plot$labels$y, "Expression (logcounts)")
+})
+
+test_that("Error with bad input", {
     expect_error(plot_marker_express(sce = sce_DLPFC_example, cellType_col = "not_there", stat = marker_test, cell_type = "Astro"))
     expect_error(plot_marker_express(sce = sce_DLPFC_example, stat = marker_test, cell_type = "not_there"))
 })
