6e58f1f8c8dddd2e147034c0ab14f4e54bcf0e49
Louise Huuki <lahuuki@gmail.com>
2025-02-17 10:33:22 +0000

    Add test for make_test_sce

---
+++ tests/testthat/test-make_test_sce.R
@@ -0,0 +1,6 @@
+test_that("make expected test sce", {
+  test_sce <- make_test_sce(n_cell = 100, n_gene = 100, n_cellType = 4, n_donor = 2)
+  expect_s4_class(test_sce, "SingleCellExperiment")
+  expect_equal(ncol(test_sce), 100)
+  expect_equal(nrow(test_sce), 100)
+})
