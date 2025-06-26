1e5a837f007eb8e8aecc03593220e8fc2e5f8cf9
lcolladotor <lcolladotor@gmail.com>
2025-06-26 13:18:58 -0400

    Attempt to solve https://github.com/LieberInstitute/DeconvoBuddies/actions/runs/15907951917/job/44867749445#step:15:48 with DelayedMatrixStats. Based on https://www.bioconductor.org/packages/release/bioc/vignettes/DelayedMatrixStats/inst/doc/DelayedMatrixStatsOverview.html#supported-methods, this should do it.

--- R/get_mean_ratio.R
+++ R/get_mean_ratio.R
@@ -73,7 +73,7 @@
 #' @importFrom purrr map
 #' @importFrom purrr map2
 #' @importFrom MatrixGenerics rowMedians
-#' @importFrom sparseMatrixStats rowMedians
+#' @importFrom DelayedMatrixStats rowMedians
 #' @importFrom MatrixGenerics rowMeans
 get_mean_ratio <- function(sce,
     cellType_col,
